605bb4024a90292dad9ca858f65681ea8c6fa40f
Murat TUNA <41265396+orientune@users.noreply.github.com>
2023-12-18 18:10:52 +0100

    Create Creating Classes & Objects.java

---
+++ Creating Classes & Objects.java
@@ -0,0 +1,14 @@
+public class Main {
+   public static void main(String[] args) {
+       
+       //create a Loading object with the same name
+       
+       
+       loading.LoadingMessage();
+   }
+}
+
+class loading {
+     static void LoadingMessage() {System.out.println("Loading");}
+     
+}
